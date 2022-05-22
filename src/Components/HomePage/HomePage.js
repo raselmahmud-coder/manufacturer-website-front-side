5c58e784ca2a39746a1692e55d09b75b9444df33
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-05-23 01:38:05 +0800

    latest post section added

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import BusinessSummary from "./BusinessSummary/BusinessSummary";
 import DiscountSection from "./DiscountSection/DiscountSection";
 import HeroSection from "./HeroSection/HeroSection";
+import LatestPosts from "./LatestPosts/LatestPosts";
 import Reviews from "./Reviews/Reviews";
 import ToolsSection from "./ToolsSection/ToolsSection";
 import TrustedInProduction from "./TrustedInProduction/TrustedInProduction";
@@ -14,7 +15,8 @@ const HomePage = () => {
       <Reviews />
       <DiscountSection />
       <TrustedInProduction />
-      <BusinessSummary/>
+      <BusinessSummary />
+      <LatestPosts />
     </>
   );
 };
--- src/Components/HomePage/LatestPosts/LatestPosts.js
+++ src/Components/HomePage/LatestPosts/LatestPosts.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import VscDebugStart from "react-icons/vsc"
+import React from "react";
+import { VscDebugStart } from "react-icons/vsc";
+import blog1 from "../../../images/blog-1.jpg";
+
 const LatestPosts = () => {
-    return (
-        <>
-            <h1 className="text-5xl text-center text-lime-700">
-                <i><VscDebugStart/></i>
-                Latest blogs
-                <i></i>
-            </h1>   
-        </>
-    );
+  return (
+    <>
+      <div className="lg:my-36 my-20">
+        <h1 className="text-5xl uppercase flex items-center justify-center text-lime-700 my-5">
+          <i className="inline-block">
+            <VscDebugStart />
+          </i>
+          Latest blogs
+          <i className="inline-block">
+            <VscDebugStart />
+          </i>
+        </h1>
+        <div className="lg:grid lg:grid-cols-3 grid-cols-1 gap-8 px-5">
+          <div className="block">
+            <img src={blog1} alt="" />
+            <div className="flex justify-between my-2">
+              <p>{new Date().toDateString()} </p>
+              <span className="font-bold text-green-300">by Rasel Mahmud</span>
+            </div>
+            <p className="text-xl my-2">
+              Biten demons lector in henderit in vulp no...
+            </p>
+            <button className="uppercase text-2xl hover:text-red-300 p-3 rounded bg-lime-200">
+              read more
+            </button>
+          </div>
+          <div className="block">
+            <img src={blog1} alt="" />
+            <div className="flex justify-between my-2">
+              <p>{new Date().toDateString()} </p>
+              <span className="font-bold text-green-300">by Rasel Mahmud</span>
+            </div>
+            <p className="text-xl my-2">
+              Biten demons lector in henderit in vulp no...
+            </p>
+            <button className="uppercase text-2xl hover:text-red-300 p-3 rounded bg-lime-200">
+              read more
+            </button>
+          </div>
+          <div className="block">
+            <img src={blog1} alt="" />
+            <div className="flex justify-between my-2">
+              <p>{new Date().toDateString()} </p>
+              <span className="font-bold text-green-300">by Rasel Mahmud</span>
+            </div>
+            <p className="text-xl my-2">
+              Biten demons lector in henderit in vulp no...
+            </p>
+            <button className="uppercase text-2xl hover:text-red-300 p-3 rounded bg-lime-200">
+              read more
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LatestPosts;
